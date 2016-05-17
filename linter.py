bad8a1a8410fe986b43accb338c7fc45fef5d634
Blake Grotewold <hello@grotewold.me>
2016-05-17 01:29:33 -0400

    Make pydocstyle happy

--- linter.py
+++ linter.py
@@ -15,7 +15,6 @@ from SublimeLinter.lint import PythonLinter, util
 
 
 class Chktex(PythonLinter):
-
     """ Provides an interface to use chktex in SublimeText with SublimeLinter3."""
 
     syntax = ('latex', 'latexing', 'latex (knitr)', 'knitr-rnw', 'latex beamer', 'latexing beamer')
